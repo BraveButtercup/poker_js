9aec6d7c0dbf61ea3f8e0b31beac9df0f9f0d8a2
bravebuttercup <batorbogi@gmail.com>
2024-06-03 19:22:20 +0200

    Deck of card games basic

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
 const CONTAINER = document.querySelector(".js-container");
-const BUTTON = document.querySelector(".js-btn");
-const chipContainer = document.querySelector(".js-chip-count-container")
-const potContainer = document.querySelector(".js-pot-container")
+const BUTTON = document.querySelector(".js-new-game-btn");
+const chipContainer = document.querySelector(".js-chip-count-container");
+const potContainer = document.querySelector(".js-pot-container");
+const betArea = document.querySelector(".js-bet-area");
+const betSlider = document.querySelector("#bet-amount");
+const betSliderValue = document.querySelector(".js-slider-value");
+const betButton = document.querySelector(".js-bet-button")
 
 //program state
 
-let deckID = null;
-let playerCards = [];
-let playerChips = 100;
-let computerChips = 100;
-let pot = 0;
+// let deckID = null;
+// let playerCards = [];
+// let playerChips = 100;
+// let computerChips = 100;
+// let pot = 0;
 
+let {
+    deckID,
+    playerCards,
+    playerChips,
+    computerChips,
+    pot
+} = getInitialState();
+
+function getInitialState() {
+    debugger;
+    return {
+        deckID: null,
+        playerCards: [],
+        playerChips: 100,
+        computerChips: 100,
+        pot: 0,
+    };
+}
+
+function canBet() {
+    return playerCards.length === 2 && playerChips > 0 && pot === 0;
+
+}
+
+function renderSlider() {
+    debugger;
+    if (canBet()) {
+        betArea.classList.remove(".invisible");
+        betSlider.setAttribute("max", playerChips);
+        betSliderValue.innerText = betSlider.value;
+    } else {
+        betArea.classList.add(".invisible");
+    }
+}
 
 function renderPlayerCards() {
 
@@ -32,10 +70,13 @@ function renderPot() {
     `
 }
 function render() {
+    debugger;
     renderPlayerCards();
     renderChips();
     renderPot();
-}
+    renderSlider();
+};
+
 async function drawAndRenderPlayerCards() {
     if (deckID == null) return;
     try {
@@ -48,7 +89,8 @@ async function drawAndRenderPlayerCards() {
         console.error('Error drawing and rendering player cards:', error);
     }
 
-}
+};
+
 function startGame() {
 
     fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
@@ -60,5 +102,16 @@ function startGame() {
         })
 };
 
+function performBet() {
+    debugger;
+    const betValue = betSliderValue;
+    pot += betValue;
+    playerChips -= betValue;
+    render();
+}
 
 BUTTON.addEventListener('click', startGame);
+betSlider.addEventListener('change', render);
+betButton.addEventListener('click', performBet);
+getInitialState();
+render();
