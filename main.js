717de435bc05fc946bafa1878e28c0cc4111bffb
bravebuttercup <batorbogi@gmail.com>
2024-06-06 19:10:51 +0200

    Call or Fold strategy

--- main.js
+++ main.js
@@ -16,37 +16,46 @@ const betButton = document.querySelector(".js-bet-button")
 // let pot = 0;
 
 let {
+    playerBetPlaced,
     deckID,
     playerCards,
     playerChips,
     computerChips,
-    pot
+    computerCards,
+    pot,
+
 } = getInitialState();
 
 function getInitialState() {
-    debugger;
+
     return {
+        playerBetPlaced: false,
         deckID: null,
+        computerCards: [],
         playerCards: [],
         playerChips: 100,
         computerChips: 100,
         pot: 0,
-    };
+    }
 }
+function initializeGame() {
 
+    ({ playerChips, computerChips, pot } = getInitialState());
+    betSlider.value = 0;
+}
 function canBet() {
-    return playerCards.length === 2 && playerChips > 0 && pot === 0;
+    return playerCards.length === 2 && playerChips > 0 && playerBetPlaced == false;
 
 }
 
 function renderSlider() {
-    debugger;
+
     if (canBet()) {
-        betArea.classList.remove(".invisible");
+        betArea.classList.remove("invisible");
         betSlider.setAttribute("max", playerChips);
         betSliderValue.innerText = betSlider.value;
     } else {
-        betArea.classList.add(".invisible");
+        betArea.classList.add("invisible");
     }
 }
 
@@ -70,7 +79,7 @@ function renderPot() {
     `
 }
 function render() {
-    debugger;
+
     renderPlayerCards();
     renderChips();
     renderPot();
@@ -82,7 +91,6 @@ async function drawAndRenderPlayerCards() {
     try {
         let response = await fetch(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`);
         let data = await response.json();
-
         playerCards = data.cards;
         render();
     } catch (error) {
@@ -90,9 +98,14 @@ async function drawAndRenderPlayerCards() {
     }
 
 };
-
-function startGame() {
-
+function postBlinds() {
+    playerChips -= 1;
+    computerChips -= 2;
+    pot += 3;
+    render();
+}
+function startHand() {
+    postBlinds();
     fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
         .then(r => r.json())
         .then(feldolgoz => {
@@ -100,18 +113,55 @@ function startGame() {
             drawAndRenderPlayerCards();
 
         })
+}
+function startGame() {
+    initializeGame();
+    startHand();
+
 };
 
+function shouldComputerCall() {
+    if (computerCards.length !== 2)
+        return false;
+    const card1Code = computerCards[0].code;
+    const card2Code = computerCards[1].code;
+    const card1Value = card1Code[0];
+    const card2Value = card2Code[1];
+    const card1Suit = card1Code[1];
+    const card2Suit = card2Code[1];
+
+    return card1Value === card2Value ||
+        ['0', 'J', 'Q', 'K', 'A'].includes(card1Value) ||
+        ['0', 'J', 'Q', 'K', 'A'].includes(card2Value) ||
+        (
+            card1Suit === card2Suit &&
+            Math.abs(Number(card1Value) - Number(card2Value)) <= 2
+        );
+}
+
+function computerMoveAfterBet() {
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
+        .then(response => response.json())
+        .then(function (response) {
+            computerCards = response.cards;
+            alert(shouldComputerCall() ? 'Call' : 'Fold');
+            console.log(computerCards);
+        });
+    //render();
+}
+
 function performBet() {
-    debugger;
-    const betValue = betSliderValue;
+
+    const betValue = Number.parseInt(betSlider.value);
     pot += betValue;
     playerChips -= betValue;
+    playerBetPlaced = true;
     render();
+    computerMoveAfterBet();
 }
 
 BUTTON.addEventListener('click', startGame);
 betSlider.addEventListener('change', render);
 betButton.addEventListener('click', performBet);
-getInitialState();
+initializeGame();
 render();
